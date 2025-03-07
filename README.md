3a1f0d68e23fdf83fb7902e1fef878107d18945c
bkarpowicz3 <brianna.karpowicz@gmail.com>
2025-03-07 15:28:35 -0500

    polished through imports

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # nomad
 Code for Nonlinear Manifold Alignment with Dynamics (NoMAD) detailed in Karpowicz et al., Nat Comms in press
+
+conda create -n nomad python=3.7.7
+conda activate nomad 
+conda install -c nvidia cudatoolkit=10.0 
+conda install -c nvidia cudnn=7.6
+cd lfads_tf2
+pip install -e .
+cd ..
+cd nomad
+pip install -e .
--- nomad/setup.py
+++ nomad/setup.py
@@ -1,8 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='nomad',
     version='0.0.0',
-    packages=['nomad'],
+    packages=find_packages(),
+    install_requires=[
+        "numba<0.53",
+        "lfads_tf2==0.1.0",
+        "protobuf==3.19"
+    ],
     author="Systems Neural Engineering Lab",
 )
